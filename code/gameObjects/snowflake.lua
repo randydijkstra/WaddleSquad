86b8a578363e5e8ddc6bc594afa1dcb323eed895
Randy Dijkstra <randydijkstra92@gmail.com>
2014-10-08 16:29:29 +0200

    added new sprite sheets, updated snowflake
    there is still a bug though…

--- code/gameObjects/snowflake.lua
+++ code/gameObjects/snowflake.lua
@@ -2,13 +2,13 @@ function createSnowflake(x, y, snowflaketype)
   local tileDeck = iif( snowflaketype == "small",
     engine:loadTileDeck( 
       "assets/sprites/special/Sneeuwvlok-spreadsheet.png", 
-      2, 1, 
-      0, 0, 25, 25 
+      4, 1, 
+      0, 0, 32, 32 
     ),
     engine:loadTileDeck(
       "assets/sprites/special/Sneeuwvlok-big-spreadsheet.png", 
-      2, 1, 
-      0, 0, 50, 50 
+      4, 1, 
+      0, 0, 64, 64 
     )
   )
   
@@ -19,19 +19,21 @@ function createSnowflake(x, y, snowflaketype)
   snowflake.name = iif( snowflaketype == "small", "small", "big")
   
   if snowflake.name == "small" then
-    snowflake.body:addRect(0, 0, 25, 25):setSensor(true)
+    snowflake.body:addRect(0, 0, 32, 32):setSensor(true)
   else
-    snowflake.body:addRect(0, 0, 50, 50):setSensor(true)
+    snowflake.body:addRect(0, 0, 64, 64):setSensor(true)
   end
+  
+  local snowflakeAnimTable = {1, 2, 3, 4, 0}
   
   local animCurve = MOAIAnimCurve.new()
-  animCurve:reserveKeys( 2 )
+  animCurve:reserveKeys( #snowflakeAnimTable )
 
-  for i = 1, 2, 1 do
-    animCurve:setKey( i, 0.15 * i, i, MOAIEaseType.FLAT ) -- hoeveelste, tijd, index in sheet, easing type
+  for i = 1, #snowflakeAnimTable, 1 do
+    animCurve:setKey( i, 0.45 * (i-1), snowflakeAnimTable[i], MOAIEaseType.FLAT ) -- hoeveelste, tijd, index in sheet, easing type
   end
   
-  anim = MOAIAnim.new()
+  local anim = MOAIAnim.new()
   anim:reserveLinks( 1 )
   anim:setLink( 1, animCurve, snowflake.prop, MOAIProp2D.ATTR_INDEX )
   anim:setMode( MOAITimer.LOOP )
