6e65b0aee62913a3dd9ea80532b49bd8cffe5fdb
Randy Dijkstra <randydijkstra92@gmail.com>
2014-10-11 15:08:15 +0200

    some adjustment on spawn location scoreText

--- code/gameObjects/snowflake.lua
+++ code/gameObjects/snowflake.lua
@@ -41,12 +41,13 @@ function createSnowflake(x, y, snowflaketype)
   
   function snowflake:onPenguinCollision()
     --print(iif(snowflake.type == "small","+25 points","+50 points"))
+    local x, y = snowflake.prop:getLoc()
     local showAddedScore = createTextBox(x+20, y-55, 60, 30, iif(snowflake.type == "small", "+25", "+50"), 25, false)
     showAddedScore.prop:moveScl(0.4, 0.4, 1, 0.8, MOAIEaseType.EASE_IN)
-    local x, y = showAddedScore.prop:getLoc()
+    local xText, yText = showAddedScore.prop:getLoc()
     showAddedScore.prop:seekLoc(
-      x, 
-      y + 20,
+      xText, 
+      yText + 20,
       1,
       1.5,
       MOAIEaseType.EASE_IN
