958ff5d4937ba4abe4afa96da5ac1ce63a21bf3c
Randy Dijkstra <randydijkstra92@gmail.com>
2014-10-11 21:40:06 +0200

    brought back disabled game over rule

--- code/gameObjects/gameStats.lua
+++ code/gameObjects/gameStats.lua
@@ -44,8 +44,8 @@ function createGameStats(levelName, defaultScore, defaultTimer)
       self:gameOver()
     elseif self.time == 0 and self.penguinsFinished > 0 then
       self:levelComplete()
-    --elseif self.penguinsOnScreen <= 0 and self.score < 300 then
-    --  self:gameOver()
+    elseif self.penguinsOnScreen <= 0 and self.score < 100 then
+      self:gameOver()
     elseif self.penguinsOnScreen <= 0 and self.penguinsLeft <= 0 and self.score > 0 then
       self:levelComplete()    
     elseif self.penguinsOnScreen <= 0 and self.penguinsLeft <= 0 and self.score < 0 then
