56a135c82b9e71f3769eaf6352c1b46a418e0a88
Mark <105424@live.nl>
2014-10-20 21:19:34 +0200

    Refactored tile

--- code/gameObjects/stage/level/tile.lua
+++ code/gameObjects/stage/level/tile.lua
@@ -10,32 +10,11 @@ function createTile(tileset, index, x, y, isHard)
       tileset.tileheight
   )  
   
-  --local tile = createDrawableGameObject(x, y, deck )
-  
-  local tile
-  if isHard then
-    tile = createDrawableGameObject(x, y, deck )
-  else
-    tile = createDrawableGameObject(x, y, deck )
-  end
-  
-  --[[
-    Somehow this doesn't work
-    local tile = iif( 
-      isHard,
-      createCollidingGameObject(x, y, deck, MOAIBox2DBody.STATIC ),
-      createDrawableGameObject(x, y, deck )
-    )  
-  --]]
+  local tile = createDrawableGameObject(x, y, deck )
   
   table.insert(tile.factions, 'tiles')
   
   tile.prop:setIndex(index)
-  
-  if isHard then
-    table.insert(tile.factions, 'hardTiles')
-    tile.body:addRect( 0, 0, tileset.tilewidth, tileset.tileheight)
-  end
-  
+
   return tile
 end
